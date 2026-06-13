6a23df7a289d6b5ce2ac9e7cc31869a124c9d271
Goku-121 <pranto1320@gmail.com>
2026-06-13 16:40:57 +0600

    fix: set axios base URL

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import 'animate.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
 import "react-loading-skeleton/dist/skeleton.css";
-
 import App from './App.jsx'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5020';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
